a590d9d277cf6a65a6ec11892d8adaab66d9b75f
rgilrodr7 <rgilrodr7@gmail.com>
2013-11-13 13:17:34 +0000

    Add folder

---
+++ addpath_script.m
@@ -0,0 +1,7 @@
+%% TODO: Run this script
+clc; clear all; close all
+addpath(genpath('resources/'));
+addpath(genpath('src/'));
+
+%% Read the data, choose a patient from resources/patients/
+% main_read;
