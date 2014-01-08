a08542414bf90d0f604c7d2e51f28de5a6d47ebf
rgilrodr7@gmail.com <rgilrodr7@gmail.com@b048a76e-666d-4446-ef6b-d6271e2c3bf8>
2014-01-08 10:02:25 +0000

    Update read_dicom

--- trunk/src/read_organise/main_read.m
+++ trunk/src/read_organise/main_read.m
@@ -2,9 +2,12 @@
 %%
 %% Main reading patient
 %%
+%% Execute:
+%% -change the folder path (line 10) where the patient is. 
+%%  This folder should contain the dicomdir file.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-folder = 'resources/patients/patient-2013/';
+clc
+folder = 'resources/patients/SAPHO/';%'/media/IMATION HDD/PhD_CF/backupAxis1/MRIs/ICE 2 - 2011/fibrome/';
 
 %% Read the dicomdir and save the info in dicomdir.mat in 'folder'
 load_dicomdir(folder);
@@ -19,3 +22,4 @@ views = get_3views(dcmdir);
 
 clear dcmdirlistVal folder mat_file %dcmdir
 clc
+
--- trunk/src/read_organise/read_images.m
+++ trunk/src/read_organise/read_images.m
@@ -1,43 +1,34 @@
-function [images alpha_ch] = read_images(folder,show)
+function [images] = read_images(folder,show)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
 %% Read and show the images from
 %% a given folder.
+%% NOTE: make sure that your folder only contains the images you
+%%        you want to read and show.
+%%
 %% Inputs: 1. folder -> name of folder containing files
 %%         3. show ->  0/1 = show/not show the images
 %%
-%% Output: 1. images -> matrix containing all the imagesaquest
+%% Output: 1. images -> matrix containing all the images
 %%
+%% Execute:
+%% - images = read_images('resources/images/',1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 dcm = dir(folder);
 
 for i=3:size(dcm,1)
-    
-    if strcmp(dcm(i).name(end-7:end),'mask.png')
         
-        filename = strcat(folder,dcm(i).name);
-        [im map alpha] = imread(filename);
+    filename = strcat(folder,dcm(i).name);
+    im = imread(filename);
 
-%         [r c] = find(im(:,:,1) == 0 & im(:,:,2) == 0 & im(:,:,3) == 0 );
-%         
-%         im(:,:,4) = zeros(size(im,1),size(im,2));
-%         
-%         for j=1:length(r)
-%             im(r(j),c(j),1:3) = [255 255 255];
-%             im(:,:,4) = 1;
-%         end
-%         images{i-2} = im(:,:,1:3).*[im(:,:,4);im(:,:,4);im(:,:,4)];
+    images{i-2} = im;
 
-        images{i-2} = im;
-        alpha_ch{i-2} = alpha;
-        
-        if show == 1
-            h = imshow(images{i-2});title(['Image: ',dcm(i).name]);
-            set(h,'AlphaData',alpha_ch{i-2});
-            pause;
-            close;
-        end
+    if show == 1
+        h = imshow(images{i-2});title(['Image: ',dcm(i).name]);
+        pause;
+        close;
     end
+
 end
 
 if show == 1
