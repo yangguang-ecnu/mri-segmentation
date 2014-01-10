302e5438dd7b625b23f51a3779191ac02e8d8c7c
rgilrodr7 <rgilrodr7@gmail.com>
2014-01-10 10:42:52 +0000

    Update manual_seg

--- src/manual_seg/contest.m
+++ src/manual_seg/contest.m
@@ -30,7 +30,7 @@ for k = 7:16%1:size(views.axial,3)
                r = str2num(input('Press 1 whenever you are ready  ','s'));
             end
 
-             h = impoly;
+            h = impoly;
             h_position = getPosition(h);
             [Vertices Lines] = contour(h_position);
 
@@ -39,7 +39,7 @@ for k = 7:16%1:size(views.axial,3)
 
             tmp = poly2mask(vert{k,i}(:,1),vert{k,i}(:,2),rows,cols);
 
-            tmp = imdilate(tmp,ones(3,3));
+            %tmp = imdilate(tmp,ones(3,3));
 
             I_texture(:,:,k) = I_texture(:,:,k) | tmp;
 
