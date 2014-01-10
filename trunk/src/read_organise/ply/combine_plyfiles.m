07a418c3f1c45733d4c264f7b3ad7e60a6851193
rgilrodr7@gmail.com <rgilrodr7@gmail.com@b048a76e-666d-4446-ef6b-d6271e2c3bf8>
2014-01-10 10:43:25 +0000

    PLY files

---
+++ trunk/src/read_organise/ply/combine_plyfiles.m
@@ -0,0 +1,110 @@
+function [faces,vertices] = combine_plyfiles(ply_ax,ply_sag,ply_cor,combine_ply)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%%  Given the 3 ply files of the contours of the 3 different views 
+%%  combine all the vertex and calculate the new mesh.
+%%
+%%  NOTE: the coordinate points in the ply files (x,y,z) are in the 3D space
+%%        i.e, in the RCS !
+%%
+%%  Inputs:  1. ply_ax -> (string) ply file containing the vertex 
+%%                        of the segmented organ, axial view 
+%%           2. ply_sag -> (string) ply file containing the vertex 
+%%                        of the segmented organ, sagittal view 
+%%           3. ply_cor -> (string) ply file containing the vertex 
+%%                        of the segmented organ, coronal view 
+%%           4. combine_ply(optional) -> (string) path of the ply file to save the 
+%%                                   vertex and faces information
+%% Outputs:  1. faces -> Nfx3 matrix, where each row is a face and the columns
+%%                              correspond to the vertex of this face
+%%           2. vertices -> Nv x 3 matrix with rows number of contour points, 
+%%                    and columns (x,y,z) coordinates
+%%
+%% EXECUTE
+%%
+%% contour2mesh('resources/manual_seg/LASSALAS/lassalas_uterurs_cor2.mat',views.coronal_info,'lassalas_cor.ply');
+%% contour2mesh('resources/manual_seg/LASSALAS/lassalas_uterurs_ax2.mat',views.axial_info,'lassalas_ax.ply');
+%% contour2mesh('resources/manual_seg/LASSALAS/lassalas_uterurs_sag2.mat',views.sagittal_info,'lassalas_sag.ply');
+%% combine_plyfiles('lassalas_ax.ply','lassalas_sag.ply','lassalas_cor.ply');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+save_file = 1;
+if nargin < 4
+    save_file = 0;
+end
+
+
+[va,fa] = read_ply(ply_ax);
+[vs,fs] = read_ply(ply_sag);
+[vc,fc] = read_ply(ply_cor);
+
+var(:,1) = [va(:,1);vs(:,1);vc(:,1)];
+var(:,2) = [va(:,2);vs(:,2);vc(:,2)];
+var(:,3) = [va(:,3);vs(:,3);vc(:,3)];
+
+[t] = MyCrust(var);
+
+FV.vertices = var;
+FV.faces = t;
+
+%% plot of the output triangulation
+figure(1)
+hold on
+title('Output Triangulation','fontsize',14)
+axis equal
+trisurf(t,var(:,1),var(:,2),var(:,3),'facecolor','c','edgecolor','b');camlight
+%patch(FV,'FaceColor',[0 1 1],'EdgeColor','interp'); view(3); camlight
+hold on
+
+%% plot the smooth triangulation
+
+FV2 = smoothpatch(FV,1,5);
+
+faces = FV2.faces;
+vertices = FV2.vertices;
+
+figure(2)
+hold on
+title('Output Smooth Triangulation','fontsize',14)
+axis equal
+trisurf(FV2.faces,FV2.vertices(:,1),FV2.vertices(:,2),FV2.vertices(:,3),'facecolor','c','edgecolor','b','edgealpha',0,'facelighting','flat');camlight
+%patch(FV2,'FaceColor',[0 1 1]); view(3); camlight %'EdgeAlpha',0
+hold on
+
+if save_file
+    
+    fileId = fopen(combine_ply, 'wt');
+    
+    % writing the header
+    fprintf(fileId, 'ply\n');
+    fprintf(fileId, 'format ascii 1.0\n');
+    fprintf(fileId, 'comment created by safir\n');
+    
+    % writing the elements
+    fprintf(fileId, 'element vertex %d\n', size(FV2.vertices,1));%var
+    
+    fprintf(fileId, 'property float32 x\n');
+    fprintf(fileId, 'property float32 y\n');
+    fprintf(fileId, 'property float32 z\n');
+    
+    fprintf(fileId, 'element face %d\n', size(FV2.faces,1));%t
+    fprintf(fileId, 'property list int32 int32 vertex_indices\n');
+    
+    fprintf(fileId, 'property uchar red\n');
+    fprintf(fileId, 'property uchar green\n');
+    fprintf(fileId, 'property uchar blue\n');
+    
+    fprintf(fileId, 'end_header\n');
+    
+    % writing the vertexes
+    
+    for i=1:size(FV2.vertices,1)
+        fprintf(fileId, '%f %f %f \n', FV2.vertices(i,1),FV2.vertices(i,2),FV2.vertices(i,3));
+    end
+    
+    % writing the faces
+    for i = 1 : size(FV2.faces,1)
+        fprintf(fileId, '3 %d %d %d %u %u %u\n', int32(FV2.faces(i,1)), int32(FV2.faces(i,2)), int32(FV2.faces(i,3)), 0, 255, 255);
+    end
+    fclose(fileId);
+    
+end
