0e713f8c44c9fa5e1fcb2ffb33ea48c220137a6f
rgilrodr7 <rgilrodr7@gmail.com>
2014-01-17 08:09:23 +0000

    Update Miscellaneous

---
+++ src/miscellaneous/bilinear_interpolation.m
@@ -0,0 +1,19 @@
+function val = bilinear_interpolation(i,j,neig)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% Bilinear interpolation given (i,j) pixel, and the four values used for it
+%%
+%% Inputs:  1. i -> row coordinate of the pixel (is not a natural)
+%%          2. j -> col coordinate of the pixel (is not a natural)
+%%          3. neig -> 2 x 2 matrix containing the gray values of the four
+%%                     neighborhood pixels 
+%%                     [I(0,0) I(0,1);I(1,0) I(1,1)]
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+i = i - floor(i);
+j = j - floor(j);
+
+val = [1-i i] * neig * [1-j j]';
+
+
