35b4a879884b852e7e369bb0777932fbcfae5083
rgilrodr7@gmail.com <rgilrodr7@gmail.com@b048a76e-666d-4446-ef6b-d6271e2c3bf8>
2014-02-17 13:57:24 +0000

    DE

--- trunk/src/optimization/DE/optimizationWithDE.m
+++ trunk/src/optimization/DE/optimizationWithDE.m
@@ -2,7 +2,7 @@ function [xmin, tfxmin] = optimizationWithDE(show_output, n_points, ...
     target,outer_shift_cart_op,movements_min, movements_max, movements_mean, PENALTIES, generations, populationSize)
 
 %% DIFFERENTIAL EVOLUTION PARAMETERS
-S_struct.I_NP         = 30; % 64 150
+S_struct.I_NP         = 50; % 64 150
 S_struct.F_weight     = 0.7; % 0.7
 S_struct.F_CR         = 0.9;%0.8 o 0.9?? % change
 S_struct.I_D          = n_points; % dimensionalidad ;
@@ -20,4 +20,4 @@ end
 S_struct.I_plotting   = 1;
 
 %de_find(0,target,outer_shift_cart_op,movements_min, movements_max, movements_mean, PENALTIES);%[27 0.055 1]);
-[xmin,tfxmin]=deopt('myfun_unc2',S_struct); %deopt_find_mask
+[xmin,tfxmin]=deopt('myfun_unc2_t',S_struct); %deopt_find_mask myfun_unc2
