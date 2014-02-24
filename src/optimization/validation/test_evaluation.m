f54a9f4216351609f9a98bf39effc94b40e422c3
rgilrodr7 <rgilrodr7@gmail.com>
2014-02-24 09:01:19 +0000

    Validation

--- src/optimization/validation/test_evaluation.m
+++ src/optimization/validation/test_evaluation.m
@@ -2,9 +2,10 @@ magnitude = [0 2 3 4 5 6 7 8];
 root = 'deform_';
 
 for i=1:length(magnitude)
-    disp(['magnitude ', num2str(magnitude(i))])
+    
+    disp(['Deformation Magnitude ', num2str(magnitude(i))])
     save_name = strcat(root,num2str(magnitude(i)),'.mat');
     load_name = strcat('def_',num2str(magnitude(i)),'.mat');
-    main_evaluation(save_name,load_name,dcmdir);
+    main_evaluation(save_name, load_name, dcmdir, opt_im_ax, opt_im_sag, opt_im_cor);
     
 end
