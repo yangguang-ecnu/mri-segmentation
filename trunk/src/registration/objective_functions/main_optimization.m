aac1bdfedf3db0461c2af6b09b96fabd26763d13
rgilrodr7@gmail.com <rgilrodr7@gmail.com@b048a76e-666d-4446-ef6b-d6271e2c3bf8>
2014-05-05 16:03:14 +0000

    Add new global variables and the error measurement

--- trunk/src/registration/objective_functions/main_optimization.m
+++ trunk/src/registration/objective_functions/main_optimization.m
@@ -1,12 +1,22 @@
 clc;
 close all;
 
-save_name = 'giraud.mat';
+%% Change the name of the file for saving the results
+save_name = 'lassalas.mat';
 
 %% Set up the parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp('-----------------------------------')
 disp('--------- Set up parameters -------')
 disp('-----------------------------------')
+
+global optimizer
+
+%% Set up the number of points per intersectin 't', 
+%% 'lambda' for the regularization term
+%% 'nx, ny, nz' for the grid dimensions
+optimizer.t     = 54;
+optimizer.lambda = .01;
+optimizer.nxyz  = [5 5 5];
 
 if ~exist('set_up','var')
     set_parameters;
@@ -17,18 +27,19 @@ disp('-----------------------------------')
 disp('--------- Start Optimization  -----')
 disp('-----------------------------------')
 
-global optimizer
-
+%% Using gradient descent
 optimizer.steepestdescent = 1;
-optimizer.views      = 3; %% provide the number of views used for the optimization, 2 or 3
 optimizer.grad       = 1; %% 0/1 without gradient/provide gradient
 optimizer.maxiter    = 40; %% maximum of iterations for the optimzer
 optimizer.checkderiv = 'off'; %% 'on'/'off' check or not the provided gradient with MATLAB one
-optimizer.tolfun     = 0.01; 
-optimizer.tolx       = 0.001; 
+optimizer.tolfun     = 0.1; 
+optimizer.tolx       = 0.01; 
 optimizer.con        = 0; %% 0/1 -> fminunc / fmincon
 
+%% Using metaheuristics, differential evolution
 optimizer.metaheuristic = 0;
+
+%% Set the number of views (default 3)
 optimizer.views         = 3; %% provide the number of views used for the optimization, 2 or 3
 
 start_optimization( );
@@ -38,29 +49,20 @@ start_optimization( );
 disp('--------- Compute the new images and visualization --------------')
 compute_new_images( );
 
+%% Calculate the measurements
+disp('--------- Compute error measurements --------------')
+measurements(  );
+
 %% Save the output if the name is provided
-
-global target_tri_ax
-global target_tri_sag
-global target_tri_cor
-
-global source_tri
-
-global new_axial
-global new_sagittal
-global new_coronal
-
 save(save_name, 'source_tri', 'target_tri_ax' , 'target_tri_sag', 'target_tri_cor',...
                 'axial_m','axial_m1','sag_m','sag_m1','cor_m','cor_m1', ...
                 'new_axial', 'new_sagittal', 'new_coronal', ...
-                'vol_ax', 'vol_sag' , 'vol_cor');
-
-
+                'vol_ax', 'vol_sag' , 'vol_cor',...
+                'optimizer', 't_reg', 'measure_errors');
+            
 %% Choose the images to visualize, i.e, [9 8 8] corresponds to slice 9 of the axial view,
 %% slice 8 from sagittal view and slice 8 from coronal one.
-visualization( views, [8 9 9] );
-
-
-
+show_slices = [19 9 12];
+visualization( views, show_slices );
 
 
