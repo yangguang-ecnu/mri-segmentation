c9ba099c553cdb47db08929a7636dcfe60b20c7f
rgilrodr7 <rgilrodr7@gmail.com>
2014-05-20 11:42:50 +0000

    Read dicom dir from folder and save it as views struct

--- src/read_organise/main_read.m
+++ src/read_organise/main_read.m
@@ -7,19 +7,27 @@
 %%  This folder should contain the dicomdir file.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc
-folder = 'resources/patients/GIRAUD/' ; %;ICE 2 - 2011/fibrome/
+
+start_path = 'resources/patients/';
+folder_name = uigetdir(start_path);
 
 %% Read the dicomdir and save the info in dicomdir.mat in 'folder'
-load_dicomdir(folder);
+load_dicomdir(folder_name);
 
-mat_file = strcat(folder,'dicomdir.mat');
+mat_file = strcat(folder_name,'/dicomdir.mat');
 load(mat_file);
 
+for i=1:length(dcmdir.dcmPatient.Study.Series)
+    series_des{i}.images = dcmdir.dcmPatient.Study.Series(i,1).Images;
+    series_des{i}.info   = dcmdir.dcmPatient.Study.Series(i,1).ImagesInfo;
+    name_series{i} = dcmdir.dcmPatient.Study.Series(i,1).SeriesDescription;
+end
 %% Define a struct with axial, sagittal and coronal views
-views = get_3views(dcmdir);
+% views = get_3views(dcmdir);
+views = choose_ax_sag_cor(series_des, name_series);
 
 %% Clear the rest of the data
 
-clear dcmdirlistVal folder mat_file %dcmdir
+clearvars -except views dcmdir
 clc
 
